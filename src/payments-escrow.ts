6c3bb7e88696abb427eb1d324da2e547e7801837
Carlos V <carlos.vdr@semiotic.ai>
2024-10-30 15:06:23 -0600

    feat: Create Tap V2 Subgraph

---
+++ src/payments-escrow.ts
@@ -0,0 +1,91 @@
+import {
+  CancelThaw,
+  Deposit,
+  Thaw,
+  Withdraw,
+} from "../generated/PaymentsEscrow/PaymentsEscrow"
+/* eslint-disable prefer-const */
+import { BigInt, Address } from '@graphprotocol/graph-ts';
+import {
+  Transaction,
+} from '../generated/schema'
+import { createOrLoadCollector, createOrLoadEscrowAccount, createOrLoadPayer, createOrLoadReceiver } from "./tap-utils"
+
+const ZERO_BI = BigInt.fromI32(0)
+// Todo receive actual address
+const COLLECTOR_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000001");
+
+export function handleThaw(event: Thaw): void {
+  let payer = createOrLoadPayer(event.params.payer)
+  let receiver = createOrLoadReceiver(event.params.receiver)
+  let collector = createOrLoadCollector(event.params.collector)
+  let escrow = createOrLoadEscrowAccount(event.params.payer, event.params.collector, event.params.receiver)
+
+  escrow.totalAmountThawing = event.params.tokens
+  escrow.thawEndTimestamp = event.params.thawEndTimestamp
+  escrow.payer = payer.id
+  escrow.receiver = receiver.id
+  escrow.collector = collector.id
+  escrow.save()
+
+}
+
+export function handleCancelThaw(event: CancelThaw): void {
+  // TODO: The contract/abi needs to be updated since here collector is not included and it should be
+  // This is not our fault but rather a mistake from E&N so need to wait for the to fix it
+  let escrow = createOrLoadEscrowAccount(event.params.payer, COLLECTOR_ADDRESS, event.params.receiver)
+  escrow.totalAmountThawing = ZERO_BI
+  escrow.thawEndTimestamp = ZERO_BI
+  escrow.save()
+}
+
+export function handleDeposit(event: Deposit): void {
+  let index = event.logIndex.toI32()
+  let transactionId = event.transaction.hash.concatI32(index)
+  let transaction = new Transaction(transactionId)
+  let payer = createOrLoadPayer(event.params.payer)
+  let receiver = createOrLoadReceiver(event.params.receiver)
+  let collector = createOrLoadCollector(event.params.collector)
+  let escrow = createOrLoadEscrowAccount(event.params.payer, event.params.collector, event.params.receiver)
+
+  escrow.balance = escrow.balance.plus(event.params.tokens)
+
+  transaction.type = "deposit"
+  transaction.payer = payer.id
+  transaction.receiver = receiver.id
+  transaction.collector = collector.id
+  transaction.tokens = event.params.tokens
+  transaction.escrowAccount = escrow.id
+  transaction.transactionGroupID = event.transaction.hash
+  transaction.timestamp = event.block.timestamp
+
+  transaction.save()
+  escrow.save()
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  let index = event.logIndex.toI32()
+  let transactionId = event.transaction.hash.concatI32(index)
+  let transaction = new Transaction(transactionId)
+  let payer = createOrLoadPayer(event.params.payer)
+  let receiver = createOrLoadReceiver(event.params.receiver)
+  let collector = createOrLoadCollector(event.params.collector)
+  let escrow = createOrLoadEscrowAccount(event.params.payer, event.params.collector, event.params.receiver)
+
+  escrow.balance = escrow.balance.minus(event.params.tokens)
+  escrow.totalAmountThawing = ZERO_BI
+  escrow.thawEndTimestamp = ZERO_BI
+
+  transaction.type = "withdraw"
+  transaction.payer = payer.id
+  transaction.receiver = receiver.id
+  transaction.collector = collector.id
+  transaction.tokens = event.params.tokens
+  transaction.escrowAccount = escrow.id
+  transaction.transactionGroupID = event.transaction.hash
+  transaction.timestamp = event.block.timestamp
+
+  transaction.save()
+  escrow.save()
+
+}
---
+++ src/tap-collector.ts
@@ -0,0 +1,76 @@
+import {
+  SignerAuthorized,
+  SignerRevoked,
+  SignerThawCanceled,
+  SignerThawing,
+  PaymentCollected
+} from "../generated/TapCollector/TapCollector"
+/* eslint-disable prefer-const */
+import { BigInt, Bytes } from '@graphprotocol/graph-ts'
+import { runTests } from "../tests/tap-collector.test"
+import {
+  Transaction,
+  Payer,
+  Receiver,
+  EscrowAccount,
+  Signer,
+  Collector
+} from '../generated/schema'
+import { createOrLoadEscrowAccount, createOrLoadPayer, createOrLoadReceiver, createOrLoadSigner } from "./tap-utils"
+let ZERO_BI = BigInt.fromI32(0)
+let ZERO_AD = Bytes.fromHexString('0x0000000000000000000000000000000000000000')
+
+export function handleSignerAuthorized(event: SignerAuthorized): void {
+  let signer = createOrLoadSigner(event.params.authorizedSigner)
+  signer.isAuthorized = true
+  signer.payer = event.params.payer
+  signer.thawEndTimestamp = ZERO_BI
+  signer.save()
+}
+
+export function handleSignerRevoked(event: SignerRevoked): void {
+  let signer = createOrLoadSigner(event.params.authorizedSigner)
+  signer.isAuthorized = false
+  signer.payer = event.params.payer
+  signer.thawEndTimestamp = ZERO_BI
+  signer.save()
+}
+
+export function handleSignerThawing(event: SignerThawCanceled): void {
+  let signer = createOrLoadSigner(event.params.authorizedSigner)
+  signer.payer = event.params.payer
+  signer.isAuthorized = true
+  signer.thawEndTimestamp = event.params.thawEndTimestamp
+  signer.save()
+}
+
+export function handleSignerThawCanceled(event: SignerThawing): void {
+  let signer = createOrLoadSigner(event.params.authorizedSigner)
+  signer.payer = event.params.payer
+  signer.isAuthorized = true
+  signer.thawEndTimestamp = ZERO_BI
+  signer.save()
+}
+
+export function handlePaymentCollected(event: PaymentCollected): void {
+  let index = event.logIndex.toI32()
+  let transactionId = event.transaction.hash.concatI32(index)
+  let transaction = new Transaction(transactionId)
+  let payer = createOrLoadPayer(event.params.payer)
+  let receiver = createOrLoadReceiver(event.params.receiver)
+  let escrow = createOrLoadEscrowAccount(event.params.payer, event.params.dataService, event.params.receiver)
+  let total_tokens_collected = escrow.balance.minus(event.params.tokensDataService).minus(event.params.tokensReceiver)
+  escrow.balance = total_tokens_collected
+  transaction.type = "redeem"
+  transaction.payer = payer.id
+  transaction.receiver = receiver.id
+
+  transaction.tokens = total_tokens_collected
+  transaction.paymentType = event.params.paymentType
+  transaction.escrowAccount = escrow.id
+  transaction.transactionGroupID = event.transaction.hash
+  transaction.timestamp = event.block.timestamp
+
+  transaction.save()
+  escrow.save()
+}
